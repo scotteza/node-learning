d536526c6e75480ca9a7527c2ff599924040e6f1
Scott Edwards <scotteza@gmail.com>
2018-02-05 12:07:00 +0200

    Learning about "require" and "exports".

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-console.log("Start app");
+// console.log("Starting app.js");
 
 const fs = require("fs");
 const os = require("os");
+const notes = require("./notes.js");
 
 const user = os.userInfo();
 const userName = user ? user.username : "unknown human";
 
-fs.appendFile("greetings.txt", `Howdy ${userName}\r\n`, err => {
-  if (err) {
-    console.log("Error: ", err);
-  }
-});
+console.log(notes.addNote());
+console.log(notes.add(1, -99999999));
 
-console.log("End app");
+// fs.appendFile(
+//   "greetings.txt",
+//   `Howdy ${userName}! You are ${notes.age} years old!\r\n`,
+//   err => {
+//     if (err) {
+//       console.log("Error: ", err);
+//     }
+//   }
+// );
