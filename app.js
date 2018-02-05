02dc1dc57f80b231878351592f9ef86bb3b23289
Scott Edwards <scotteza@gmail.com>
2018-02-05 16:14:59 +0200

    Setting up yargs.

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const fs = require("fs");
 const _ = require("lodash");
 const yargs = require("yargs");
-
 const notes = require("./notes.js");
 
-const argv = yargs.argv;
+// Set up yargs
+const titleArg = {
+  describe: "The title of the note",
+  demand: true,
+  alias: "t",
+  type: "string"
+};
+const bodyArg = {
+  describe: "The body of the note",
+  demand: true,
+  alias: "b",
+  type: "string"
+};
+const argv = yargs
+  .command("add", "Add a new note", {
+    title: titleArg,
+    body: bodyArg
+  })
+  .command("list", "List all notes")
+  .command("read", "Read a note", {
+    title: titleArg
+  })
+  .command("remove", "Remove a note", {
+    title: titleArg
+  })
+  .help().argv;
+
+// Read yargs
 const title = argv.title;
 const body = argv.body;
 const command = argv._[0];
 
+// Execute command
 switch (command) {
   case "add": {
     const addedNote = notes.addNote(title, body);
