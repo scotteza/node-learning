7336e4eb67093b3b971250a5731e6017bbd7a420
Scott Edwards <scotteza@gmail.com>
2018-02-05 13:16:46 +0200

    Basic lodash. Adding my vscode settings file.

--- app.js
+++ app.js
@@ -2,14 +2,45 @@
 
 const fs = require("fs");
 const os = require("os");
+const _ = require("lodash");
 const notes = require("./notes.js");
 
 const user = os.userInfo();
 const userName = user ? user.username : "unknown human";
 
-console.log(notes.addNote());
-console.log(notes.add(1, -99999999));
+/* 
+---------------------------------------------------------------------
+Calling lodash
+---------------------------------------------------------------------
+*/
 
+// isString
+// console.log(_.isString(true));
+// console.log(_.isString("Scott"));
+// console.log(_.isString(12));
+
+// uniq
+// var scott1 = { fn: "sc", ln: "ed", id: 1 };
+// var scott2 = { fn: "sc", ln: "ed", id: 2 };
+// var scott3 = scott1;
+// var arr1 = ["a",  "b",  "b",  "b",  "b",  "b",  "b",  "b",  "b",  12,  13,  14,  15,  15,  15,  15,  15,  scott1,  scott2,  scott3];
+// var arr2 = _.uniq(arr1);
+// console.log(arr1);
+// console.log(arr2);
+
+/* 
+---------------------------------------------------------------------
+Using imports from other files
+---------------------------------------------------------------------
+*/
+// console.log(notes.addNote());
+// console.log(notes.add(1, -99999999));
+
+/* 
+---------------------------------------------------------------------
+File appending
+---------------------------------------------------------------------
+*/
 // fs.appendFile(
 //   "greetings.txt",
 //   `Howdy ${userName}! You are ${notes.age} years old!\r\n`,
