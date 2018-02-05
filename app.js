92eff4136ef9700d83dbde2167ff157b8dcb8d2f
Scott Edwards <scotteza@gmail.com>
2018-02-05 13:59:52 +0200

    Added yargs.

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const fs = require("fs");
 const _ = require("lodash");
+const yargs = require("yargs");
 
 const notes = require("./notes.js");
+
+const argv = yargs.argv;
+const title = argv.title;
+const body = argv.body;
+const command = argv._[0];
+
+if (command === "add") {
+  notes.addNote(title, body);
+} else if (command === "list") {
+  notes.getAll();
+} else if (command === "read") {
+  notes.read(title);
+} else if (command === "remove") {
+  notes.remove(title);
+} else {
+  console.log("Command not recognized");
+}
--- notes.js
+++ notes.js
@@ -1,4 +1,22 @@
-module.exports.addNote = () => {
-  console.log("addNote");
-  return "New note";
+const addNote = (title, body) => {
+  console.log("Adding note", title, body);
+};
+
+const getAll = () => {
+  console.log("Getting all notes");
+};
+
+const read = title => {
+  console.log("Getting note", title);
+};
+
+const remove = title => {
+  console.log("Removing note", title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  read,
+  remove
 };
