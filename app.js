c84f1f8669f02550381b55811439966c9126ef3a
Scott Edwards <scotteza@gmail.com>
2018-02-26 13:09:25 +0200

    Taking in args for location search. Error handling added.

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const fs = require("fs");
 const fn = "C:\\Development\\googlekey.txt";
 const request = require("request");
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+    address: {
+      demand: true,
+      alias: "a",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
 
 const key = fs.readFileSync(fn);
-const searchTerm = "1301%20lombard%20street%20philadelphia";
+const searchTerm = encodeURIComponent(argv.a);
 
 const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchTerm}&key=${key}`;
-
-console.log(url);
+// console.log(url);
 
 request(
   {
@@ -15,8 +27,15 @@ request(
     json: true
   },
   (error, response, body) => {
-    if (body) {
-      console.log(JSON.stringify(body, undefined, 2));
+    if (error) {
+      console.log("There was an error");
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log("No results found");
+    } else if (body.status === "OK") {
+      // console.log(JSON.stringify(response, undefined, 2));
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
     }
   }
 );
